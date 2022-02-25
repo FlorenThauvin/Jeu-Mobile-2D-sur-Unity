0c171b398d81251bef77dc8093ec61c7d2877a7b
FlorenThauvin <florent_1999@hotmail.fr>
2022-02-25 19:29:38 +0100

    rotation perso

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -4,12 +4,25 @@ using UnityEngine;
 
 public class Movement : MonoBehaviour
 {
-    public float Speed =1.0f;
+    public float Speed =1.5f;
+    public float Jump = 5.0f;
+    private Rigidbody2D _rigidbody;
 
+    private void Start() {
+        _rigidbody = GetComponent<Rigidbody2D>();
+    }
 
     void Update()
     {
         var move = Input.GetAxis("Horizontal");
         transform.position += new Vector3(move,0,0)* Time.deltaTime * Speed;
+
+        if (!Mathf.Approximately(0,move)){
+            transform.rotation = move > 0 ? Quaternion.Euler(0,180,0):Quaternion.identity;
+        }
+
+        if(Input.GetButtonDown("Jump")&& Mathf.Abs(_rigidbody.velocity.y)<0.1f){
+            _rigidbody.AddForce(new Vector2(0,Jump),ForceMode2D.Impulse);
+        }
     }
 }
