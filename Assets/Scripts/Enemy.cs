c5583f87bfc463e3328bb854d016db41dfde6d09
FlorenThauvin <florent_1999@hotmail.fr>
2022-03-02 23:38:31 +0100

    update map + ennemi

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -15,13 +15,18 @@ public class Enemy : MonoBehaviour
     }
 
     void Update(){
+        int taille = waypoints.Length;
         Vector3  direction = target.position - transform.position;
         transform.Translate(direction.normalized * speed * Time.deltaTime , Space.World);
-
+        float last = waypoints[(destination-1+taille)%taille].position.x;
         if(Vector3.Distance(transform.position,target.position) < 0.3f){
-            destination = (destination+1) % waypoints.Length;
+            destination = (destination+1) % taille;
             target = waypoints[destination];
-            sprite.flipX = !sprite.flipX;
+            bool dirEnnemy = (last - transform.position.x)<0; 
+            bool dirEnnemy2 = transform.position.x - target.position.x<0;
+            if (dirEnnemy!=dirEnnemy2){
+                sprite.flipX = !sprite.flipX;
+            }
         }
     }
     private void OnCollisionEnter2D(Collision2D other) {        // fonction qui permet a l'ennemi de faire des dégats si il rentre en collision
